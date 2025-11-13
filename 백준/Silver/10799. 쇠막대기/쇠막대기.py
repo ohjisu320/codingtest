5f647afe7a5c2bf83e914debebc62a0ab08a9465
ohjisu320 <151099185+ohjisu320@users.noreply.github.com>
2025-11-14 02:23:11 +0900

    [Silver II] Title: 쇠막대기, Time: 104 ms, Memory: 117588 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -0,0 +1,20 @@
+# 초기설정
+import sys
+input = lambda :sys.stdin.readline().rstrip()
+
+lst = list(input())
+
+q = list()
+cnt = 0
+for idx, item in enumerate(lst):
+    if item == '(': # 레이저 or 쇠막대기
+        q.append(item)
+    elif item == ')': # 레이저 or 쇠막대기 끝
+        q.pop() # 레이저이든 쇠막대기든 둘 다 pop할 것임
+
+        if lst[idx - 1] == '(': # 레이저일 때
+            cnt += len(q)
+        elif lst[idx - 1] == ')': # 쇠막대기의 끝일 때
+            cnt += 1
+
+print(cnt)
