88ccb94f5d8c51ad9b71f55955dd2ab1b65cad47
ohjisu320 <151099185+ohjisu320@users.noreply.github.com>
2025-11-25 21:31:06 +0900

    [Silver II] Title: 연속합, Time: 104 ms, Memory: 121956 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,14 @@
+import sys
+input  = lambda :sys.stdin.readline().rstrip()
+n = int(input())
+arr = list(map(int, input().split()))
+
+
+dp =  [0] * n
+
+result = dp[0] = arr[0]
+for i in range(1, n):
+    dp[i] = max(dp[i - 1] + arr[i], arr[i])
+    result = max(result, dp[i])
+
+print(result)
