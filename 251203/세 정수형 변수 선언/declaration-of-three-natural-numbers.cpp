c259e725f3e4b94dd92a3aa72a2f2e3d08021fca
ohjisu320 <151099185+ohjisu320@users.noreply.github.com>
2025-12-03 21:50:23 +0900

    Title: 세 정수형 변수 선언, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251203/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 7;
+    int b = 23;
+    int c = 30;
+    // Please write your code here.;
+    cout << a << " + " << b << " = " << c;
+}
