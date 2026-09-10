b28705e684e0dcb80d236064570f9fbfedd169e4
ohjisu320 <151099185+ohjisu320@users.noreply.github.com>
2026-09-10 15:18:07 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.19 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,15 +1,16 @@
 def solution(new_id):
     new_id = new_id.lower()  # 1단계: 소문자로 변환 / 3단계: ..->.으로 변환
+
     answer = ''
      # 2단계: 특수문자 삭제
-    for delete in ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "=", "+", "[", "{", "]", "}", ":", "?", ",",
-                     "<", ">", "/"]:
-        new_id = new_id.replace(delete, "")
+    for id in new_id:
+        if  ord('0') <= ord(id) <= ord('9') or ord('a') <= ord(id) <= ord('z') or id in '_.-':
+            if id =='.' and answer and answer[- 1] =='.':
+                pass
+            else:
+                answer += id
 
-    while '..' in new_id:
-        new_id = new_id.replace('..', '.')
-
-    answer = new_id.strip(".")  # 4단계: 앞뒤 . 삭제
+    answer = answer.strip(".")  # 4단계: 앞뒤 . 삭제
 
     if answer == '':  # 5단계: 빈 문자열일 경우 "a"
         answer = "a"
